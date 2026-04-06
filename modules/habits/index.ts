aeb7f971dc9028290530046c6069283b8ccc1c4e
r2hu1 <newasram86@gmail.com>
2026-04-06 12:37:58 +0530

    feat: export EditHabitModal from habits module

--- modules/habits/index.ts
+++ modules/habits/index.ts
@@ -1,3 +1,4 @@
 export { HabitCard } from "./HabitCard";
 export { HabitCheckButton } from "./HabitCheckButton";
 export { Heatmap } from "./Heatmap";
+export { EditHabitModal } from "./EditHabitModal";
